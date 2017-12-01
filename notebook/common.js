c19d61c5c15903c5a9c7d598e5cbb7eb97242051
web-leozet <listikweb@gmail.com>
2017-12-01 10:49:00 +0200

    update-011217

--- notebook/common.js
+++ notebook/common.js
@@ -3,16 +3,16 @@
 // а вот такую возвращать ​​'ehT kciuq nworb xof spmuj revo eht yzal .god')
 
 function rev (p) {
-  return p.split(' ')
-  .map(function (x) {
-        return x.split('').reverse().join('');
-  }).join(' ');
- }
+	return p.split(' ')
+	.map(function (x) {
+		return x.split('').reverse().join('');
+	}).join(' ');
+}
 
- console.log(rev("The quick brown fox jumps over the lazy dog."));
+console.log(rev("The quick brown fox jumps over the lazy dog."));
 
 
- var c = ["September","October",[1,["ra", "ka", "two"],2,5],"one","November","December","January","Fabruar",
+var c = ["September","October",[1,["ra", "ka", "two"],2,5],"one","November","December","January","Fabruar",
 "March","April","May","June","Jull","August"];
 console.log(c[2][3]);
 
@@ -35,7 +35,7 @@ document.write(list.join(", "));
 
 
 var a = [];
-	var input = prompt("Введите новый елемент массива(списка)", "");
+var input = prompt("Введите новый елемент массива(списка)", "");
 while (input != null) {a = a.concat(input);
 	input = prompt("Введите ещё один елемент массива(списка)", "");
 }	
@@ -49,20 +49,20 @@ document.write("</li></ul>");
 //функция для создания массива 
 
 function newArrayLine(count) {
-    var arr = [];
-    for (var i = 0; i < count; i++) {
-        arr[i] = i;
-    }
-    return arr;
+	var arr = [];
+	for (var i = 0; i < count; i++) {
+		arr[i] = i;
+	}
+	return arr;
 }
 
 console.log(newArrayLine(20));
 
 
-  //arguments  Максимальное значение аргументов массива
+  // Максимальное значение аргументов массива
 
-   function max() {
- 	var maxValue = Number.NEGATIVE_INFINITY; //  = или arguments[0]
+  function max() {
+ 	var maxValue = arguments[0]; //  = или arguments[0]
  	for (var i = 0; i < arguments.length; i++) 
  		if (arguments[i] > maxValue) 
  			maxValue = arguments[i];
@@ -77,19 +77,19 @@ console.log(newArrayLine(20));
  	// Рекурсия - это функция которая сама себя вызывает
 
 
-function count(counter) {
-	counter--;
+ 	function count(counter) {
+ 		counter--;
 
-	document.write("<img src = '../img/1.jpg' width = '50' height = '50'>" + counter + "<br/>");
-	if (counter != 0) {
-		count(counter);
-	}
-	document.write("<img src = '../img/2.jpg' width = '50' height = '50'>" + counter + "<br/>");
-}
+ 		document.write("<img src = '../img/1.jpg' width = '50' height = '50'>" + counter + "<br/>");
+ 		if (counter != 0) {
+ 			count(counter);
+ 		}
+ 		document.write("<img src = '../img/2.jpg' width = '50' height = '50'>" + counter + "<br/>");
+ 	}
 
-count(5);
+ 	count(5);
 
-document.write("<hr/>");
+ 	document.write("<hr/>");
 
 
 //callback function
@@ -122,21 +122,21 @@ var minus = function  (a,b) {
 
 			//object
 
-var insts = {
-	propertyA: 100,
-	propertyB: 200
-};
+			var insts = {
+				propertyA: 100,
+				propertyB: 200
+			};
 
-console.log(insts.propertyA);
-console.log(insts.propertyB);
+			console.log(insts.propertyA);
+			console.log(insts.propertyB);
 
-insts.propertyA = 500;
-insts.propertyB = 1000;
+			insts.propertyA = 500;
+			insts.propertyB = 1000;
 
-console.log(insts.propertyA);
-console.log(insts.propertyB);
-insts["propertyB"] = 2000;
-console.log(insts.propertyB);
+			console.log(insts.propertyA);
+			console.log(insts.propertyB);
+			insts["propertyB"] = 2000;
+			console.log(insts.propertyB);
 
 
 
